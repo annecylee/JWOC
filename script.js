0092a1734623a3a636386dab0cd9ade10830d8b9
annecylee <annecylee@gmail.com>
2017-04-11 14:28:36 -0700

    Init

---
+++ script.js
@@ -0,0 +1,16 @@
+// $(window).scroll( function(){
+//   var coverElem = $(".cover");
+//   var targetElem = $("#donateRound")
+//   var bottom_of_object = coverElem.offset().top + coverElem.outerHeight();
+//   var bottom_of_window = $(window).scrollTop()
+//   console.log(bottom_of_object);
+//   console.log(bottom_of_window);
+//
+//   if( bottom_of_window > bottom_of_object ){
+//       console.log(1);
+//       targetElem.fadeIn({'opacity':'1'},500);
+//   } else {
+//       targetElem.fadeOut({'opacity':'1'},500);
+//   }
+//
+// });
