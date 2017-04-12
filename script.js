65f9cd2630ac5ad6894f22d1e0db0db2123cb8fc
annecylee <annecylee@gmail.com>
2017-04-11 22:50:07 -0700

    add srcset and js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// $(window).scroll( function(){
-//   var coverElem = $(".cover");
-//   var targetElem = $("#donateRound")
-//   var bottom_of_object = coverElem.offset().top + coverElem.outerHeight();
-//   var bottom_of_window = $(window).scrollTop()
-//   console.log(bottom_of_object);
-//   console.log(bottom_of_window);
-//
-//   if( bottom_of_window > bottom_of_object ){
-//       console.log(1);
-//       targetElem.fadeIn({'opacity':'1'},500);
-//   } else {
-//       targetElem.fadeOut({'opacity':'1'},500);
-//   }
-//
-// });
+$(function() {
+
+  $(".menu").click(function() {
+    $(".navbar-nav").toggle();
+
+  });
+
+  $(window).click(function(){
+    $(".navbar-nav").hide();
+  });
+
+  $(window).scroll(function(){
+    $(".navbar-nav").hide();
+  });
+
+  $('.menu').click(function(event){
+    event.stopPropagation();
+});
+
+});
